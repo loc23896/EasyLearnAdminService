2d3ddd1c7a6924676155a9a397bd89d55ad983e5
ITminhnh2509 <135216505+ITminhnh2509@users.noreply.github.com>
2023-12-17 15:55:07 +0700

    Update TrainerSalaryPaid.java
    Add Serializable

--- src/main/java/com/easy/learn/dto/TrainerSalaryPaid/TrainerSalaryPaid.java
+++ src/main/java/com/easy/learn/dto/TrainerSalaryPaid/TrainerSalaryPaid.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TrainerSalaryPaid {
+public class TrainerSalaryPaid implements Serializable {
     private Long id;
     private String trainerSalaryPaidId;
     private Integer weekNumber;
