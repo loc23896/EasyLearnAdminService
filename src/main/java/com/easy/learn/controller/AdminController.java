76672964f389c35efc90cf5469f52ca2aca965c4
loc23896 <loc23896@gmail.com>
2023-12-22 23:26:46 +0700

    update Pages by Loc

--- src/main/java/com/easy/learn/controller/AdminController.java
+++ src/main/java/com/easy/learn/controller/AdminController.java
@@ -2,33 +2,75 @@ package com.easy.learn.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
-    @GetMapping("/admin")
+    @GetMapping("")
     public String index (){
         return "pages/admin/index";
     }
 
-    @GetMapping("/admin/finance")
-    public String adminFinance(){
-        return "pages/admin/admin_finance";
+    @GetMapping("/finance/student")
+    public String adminFinanceStudent(){
+        return "pages/admin/finance_managements/student";
     }
 
-    @GetMapping("/admin/customer-care")
-    public String adminCustomerCare(){
-        return "pages/admin/admin_customerCare";
+    @GetMapping("/finance/trainer")
+    public String adminFinanceTrainer(){
+        return "pages/admin/finance_managements/trainer";
     }
 
-    @GetMapping("/admin/members")
-    public String adminMembers(){
-        return "pages/admin/admin_members";
+    @GetMapping("/member/student")
+    public String adminMemberStudent(){
+        return "pages/admin/member_managements/student";
     }
 
-    @GetMapping("/admin/dashboard")
+    @GetMapping("/member/trainer")
+    public String adminMemberTrainer(){
+        return "pages/admin/member_managements/trainer";
+    }
+
+    @GetMapping("/customerCare/student")
+    public String adminCustomerCareStudent(){
+        return "pages/admin/customerCare_managements/student";
+    }
+
+    @GetMapping("/customerCare/trainer")
+    public String adminCustomerCareTrainer(){
+        return "pages/admin/customerCare_managements/trainer";
+    }
+
+
+    @GetMapping("/dashboard")
     public String adminDashboard(){
         return "pages/admin/admin_dashboard";
+    }
+
+    @GetMapping("/table")
+    public String adminTable(){
+        return "pages/admin/admin_table";
+    }
+
+
+
+
+    @GetMapping("/profile")
+    public String profile(){
+        return "pages/admin/admin_profile";
+    }
+
+    @GetMapping("/profile/edit")
+    public String changeProfile(){
+        return "pages/admin/admin_profile_edit";
+    }
+
+    //admin user profile other.
+    @GetMapping("/profile-other")
+    public String adminProfileOther(){
+        return "pages/admin/admin_profile_other";
     }
 
     @GetMapping("/login")
@@ -41,23 +83,32 @@ public class AdminController {
         return "pages/loginPage/register";
     }
 
-    @GetMapping("/table")
-    public String table(){
-        return "pages/admin/admin_table";
-    }
 
-    @GetMapping("/profile")
-    public String profile(){
-        return "pages/admin/admin_profile";
-    }
 
-    @GetMapping("/profile/edit")
-    public String changeProfile(){
-        return "pages/admin/admin_profile_edit";
-    }
 
-    @GetMapping("/admin/profile-other")
-    public String adminProfileOther(){
-        return "pages/admin/admin_profile_other";
-    }
+
+
+
+
+
+
+
+
+    //    @GetMapping("/customer-care")
+//    public String adminCustomerCare(){
+//        return "pages/admin/admin_customerCare";
+//    }
+//
+//    @GetMapping("/members")
+//    public String adminMembers(){
+//        return "pages/admin/admin_members";
+//    }
+//
+
+//
+//    @GetMapping("/table")
+//    public String table(){
+//        return "pages/admin/admin_table";
+//    }
+//
 }
--- src/main/java/com/easy/learn/controller/CustomerCareController.java
+++ src/main/java/com/easy/learn/controller/CustomerCareController.java
@@ -5,32 +5,56 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/customer-care")
+@RequestMapping("/customerCare")
 public class CustomerCareController {
-    @GetMapping("")
-    public String customerCareIndex(){
-        return "pages/customerCare/index";
+
+    @GetMapping("/student/detail")
+    public String adminCustomerCareStudentDetail(){
+        return "pages/admin/customerCare_managements/detail_student";
     }
 
-    @GetMapping("/student")
-    public String customerCareStudent(){
-        return "pages/customerCare/customerCare_student";
+    @GetMapping("/student/replay")
+    public String adminCustomerCareStudentDetailReplay(){
+        return "pages/admin/customerCare_managements/detail_student_replay";
     }
 
-    @GetMapping("/trainer")
-    public String customerCareTrainer(){
-        return "pages/customerCare/customerCare_trainer";
+    @GetMapping("/trainer/detail")
+    public String adminCustomerCareTrainerDetail(){
+        return "pages/admin/customerCare_managements/detail_trainer";
     }
 
-    @GetMapping("/table")
-    public String customerCareTableIndex(){
-        return "pages/customerCare/customerCare_table";
+    @GetMapping("/trainer/replay")
+    public String adminCustomerCareTrainerDetailReplay(){
+        return "pages/admin/customerCare_managements/detail_trainer_replay";
     }
 
-    @GetMapping("/dashboard")
-    public String customerCareDashboard(){
-        return "pages/customerCare/customerCare_dashboard";
-    }
+
+
+//
+//    @GetMapping("")
+//    public String customerCareIndex(){
+//        return "pages/customerCare/index";
+//    }
+//
+//    @GetMapping("/student")
+//    public String customerCareStudent(){
+//        return "pages/customerCare/customerCare_student";
+//    }
+//
+//    @GetMapping("/trainer")
+//    public String customerCareTrainer(){
+//        return "pages/customerCare/customerCare_trainer";
+//    }
+//
+//    @GetMapping("/table")
+//    public String customerCareTableIndex(){
+//        return "pages/customerCare/customerCare_table";
+//    }
+//
+//    @GetMapping("/dashboard")
+//    public String customerCareDashboard(){
+//        return "pages/customerCare/customerCare_dashboard";
+//    }
 
 
 }
